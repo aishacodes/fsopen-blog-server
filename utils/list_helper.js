b4e91efc54f9008960586db2de16923b7df7136b
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-02-06 15:03:11 +0100

    feat: 4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,18 @@ const totalLikes = (blogs) => {
     ? 0
     : blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);
 };
+const favouriteBlog = (blogs) => {
+  const highestLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const fave = blogs.find((blog) => blog.likes === highestLikes);
+  return {
+    title: fave.title,
+    author: fave.author,
+    likes: fave.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
